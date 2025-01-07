d10273e09b716d46633b04a67573960a14efe352
Matricarin <matricarin@gmail.com>
2025-01-07 22:09:45 +0500

    commit

---
+++ Demo/WPFDemo/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace WPFDemo;
+
+public partial class App : Application
+{
+    protected override void OnExit(ExitEventArgs e)
+    {
+        base.OnExit(e);
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        var window = new MainWindow();
+        window.Show();
+    }
+}
